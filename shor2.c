52c0a09132f035957e23623ce043d3a74087afbc
adleger <ndmit@LAPTOP-I5F3TOJE.localdomain>
2026-04-09 02:34:47 +0300

    temp changes

--- shor2.c
+++ shor2.c
@@ -52,73 +52,69 @@ struct log_struct{
     int target;
 };
 
+struct log_struct logs[] = {}
 int main(int argc, char **argv) {
 
-    for(int i = 1; i+1 < argc; i+=2){
-	    char* log_file = argv[i];
-	    int repeat_count = strtol(argv[i+1], NULL, 10);
+    double total_time = 0;
+    size_t logs_length = sizeof(logs)/sizeof(logs[0]);
+    for(int i = 0; i < logs_length; i++){
+   
+      // 1. читаем log_struct
+      struct log_struct log = logs[i];
 
-    FILE *file = fopen(log_file, "rb");
-    if (!file) {
-        perror("Can't open file");
-        return 1;
-    }
+      int c1 = log.control1;
+      int c2 = log.control2;
+      int tg = log.target;
 
-    quantum_reg* reg = malloc(sizeof(quantum_reg));
-    // 1. читаем log_struct
-    struct log_struct log;
-    fread(&log, sizeof(struct log_struct), 1, file);
+      // 2. читаем quantum_reg_struct (но указатели игнорируем!)
+      quantum_reg* reg = malloc(sizeof(quantum_reg));
+      struct quantum_reg_struct tmp_reg = log->reg;
+      
+      // копируем только полезные поля
+      reg->width = tmp_reg.width;
+      reg->size  = tmp_reg.size;
+      reg->hashw = tmp_reg.hashw;
 
-    int c1 = log.control1;
-    int c2 = log.control2;
-    int tg = log.target;
+      // 3. выделяем память
+      reg->node = (quantum_reg_node*)malloc(reg->size * sizeof(quantum_reg_node));
+      reg->hash = (int*)malloc(reg->hashw * sizeof(int));
 
-    // 2. читаем quantum_reg_struct (но указатели игнорируем!)
-    struct quantum_reg_struct tmp_reg;
-    fread(&tmp_reg, sizeof(struct quantum_reg_struct), 1, file);
+      // 4. читаем node массив
+      //fread(reg->node, sizeof(struct quantum_reg_node_struct), reg->size, file);
+      memcpy(reg->node, tmp_reg->node, reg->size);
 
-    // копируем только полезные поля
-    reg->width = tmp_reg.width;
-    reg->size  = tmp_reg.size;
-    reg->hashw = tmp_reg.hashw;
+      // 5. читаем hash массив
+      //fread(reg->hash, sizeof(int), reg->hashw, file);
+      memcpy(reg->hash, tmp_reg->hash, reg->hashw);
 
-    // 3. выделяем память
-    reg->node = (quantum_reg_node*)malloc(reg->size * sizeof(quantum_reg_node));
-    reg->hash = (int*)malloc(reg->hashw * sizeof(int));
 
-    // 4. читаем node массив
-    fread(reg->node, sizeof(struct quantum_reg_node_struct), reg->size, file);
-
-    // 5. читаем hash массив
-    fread(reg->hash, sizeof(int), reg->hashw, file);
-
-    fclose(file);
-
-    // проверка
-    //printf("%d %d %d %d %d %d\n", c1, c2, tg,
-    //      reg->width, reg->size, reg->hashw);
-   quantum_reg* temp_reg = malloc(sizeof(quantum_reg));
-    temp_reg->node = malloc(sizeof(quantum_reg_node) * reg->size);
-    temp_reg->hash = malloc(sizeof(int) * reg->hashw);
-    double start = omp_get_wtime();
-    for(int j = 0 ; j < repeat_count; j++){
+      // проверка
+      //printf("%d %d %d %d %d %d\n", c1, c2, tg,
+      //      reg->width, reg->size, reg->hashw);
+      quantum_reg* temp_reg = malloc(sizeof(quantum_reg));
+      temp_reg->node = malloc(sizeof(quantum_reg_node) * reg->size);
+      temp_reg->hash = malloc(sizeof(int) * reg->hashw);
+      double hotspot_time = 0;
+      for(int j = 0 ; j < repeat_count; j++){
         temp_reg->width = reg->width;
         temp_reg->size  = reg->size;
         temp_reg->hashw = reg->hashw;
 
-        memcpy(temp_reg->node, reg->node,
-              sizeof(quantum_reg_node) * reg->size);
+        memcpy(temp_reg->node, reg->node, sizeof(quantum_reg_node) * reg->size);
 
-        memcpy(temp_reg->hash, reg->hash,
-              sizeof(int) * reg->hashw);
+        memcpy(temp_reg->hash, reg->hash, sizeof(int) * reg->hashw);
 
+        double start = omp_get_wtime();
         quantum_toffoli(c1, c2, tg, temp_reg);
-    }
+        double end = omp_get_wtime();
+        hotspot_time += end-start;
+      }
+      total_time += hotspot_time;
 
-    free(temp_reg->hash);
-    free(temp_reg->node);
-    free(temp_reg);
-    double end = omp_get_wtime();
-    printf("%lf\n", (double)(end-start));
+      free(temp_reg->hash);
+      free(temp_reg->node);
+      free(temp_reg);
+      printf("hotspot time: %lf\n", (double)(hotspot_time));
     }
+    printf("total time: %lf\n", total_time);
 }
