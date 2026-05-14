53cfd0441151ef57e5231104becd164c6e32b2d4
adleger <ndmit@LAPTOP-I5F3TOJE.localdomain>
2026-05-14 18:47:28 +0300

    improve mean_time

--- mean_time.py
+++ mean_time.py
@@ -1,10 +1,20 @@
 from math import sqrt
+import sys
+if len(sys.argv)>1:
+    file = sys.argv[1]
+else:
+    file = "../logs/logarr.txt"
+
+if len(sys.argv)>1:
+    count = int(sys.argv[1])
+else:
+    count =10 
 
 raw_logs = []
-logs_count = 10
+logs_count = count
 for i in range(logs_count):
     raw_logs.append([])
-    with open(f"../logs/logarr{i}.txt") as f:
+    with open(file[:-4]+str(i)+file[-4:]) as f:
         lines = f.readlines()
     for j in range(len(lines)):
         raw_logs[i].append(float(lines[j]))
@@ -16,13 +26,13 @@ for i in range(len(raw_logs[0])):
     mean = 0
     mean2 = 0
     for j in range(logs_count):
-        mean += raw_logs[j][i]/10
-        mean2 += raw_logs[j][i]*raw_logs[j][i]/10
+        mean += raw_logs[j][i]/count
+        mean2 += raw_logs[j][i]*raw_logs[j][i]/count
     disp =  mean2 - mean*mean
     sigma = sqrt(disp)
     new_mean=0
     for j in range(logs_count):
-        if raw_logs[j][i] < sigma*3+mean and mean - 3*sigma < raw_logs[j][i] :
+        if raw_logs[j][i] < sigma*2.9+mean and mean - 2.9*sigma < raw_logs[j][i] :
             cur_logs.append(raw_logs[j][i])
     droped_count += logs_count - len(cur_logs)
     for j in range(len(cur_logs)):
